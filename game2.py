cafcc2e30f6771123590dd8c611881934e41174e
PoluboyarinovSI <Spoluboyarinow@yandex.ru>
2021-11-15 21:24:07 +0300

    Version_1

---
+++ game2.py
@@ -0,0 +1,43 @@
+import numpy as np
+from numpy import random
+    
+def random_preidct(number:int=1) -> int:
+    """Randomly number predictor
+
+    Args:
+       number (int, optional): Number ti predict. Defaults to 1.
+
+    Returns:
+        int: Attempts number
+    """
+    count = 0
+        
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 100)
+        if number == predict_number:
+            break
+    return count
+    
+def score_game(random_preidct) -> int:
+    """Mean attempts to predict
+
+    Args:
+        random_preidct ([type]): predict function
+
+    Returns:
+        int: Mean attempts
+    """
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 100, size=(1000)) # list of numbers
+        
+    for number in random_array:
+        count_ls.append(random_preidct(number))
+        
+    score = int(np.mean(count_ls))
+    print(f'Mean attempts to pretict numeber: {score}')
+
+
+if __name__ == '__main__':
+    score_game(random_preidct)
