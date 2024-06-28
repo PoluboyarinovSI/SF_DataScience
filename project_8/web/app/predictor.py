b5c59233d6856ab006cb3d2ce60bde66c82fcdef
Spoluboyarinow <Spoluboyarinow@yandex.ru>
2024-06-28 17:31:58 +0300

    edited predictor file

--- project_8/web/app/predictor.py
+++ project_8/web/app/predictor.py
@@ -13,26 +13,6 @@ with open('./models/all_items.pkl', 'rb') as f:
 with open('./models/all_users.pkl', 'rb') as f:
     all_users = pickle.load(f)
 
-'''
-def get_inner_model_visitorid(visitorid):
-    return visitorid_mapping.get(visitorid)
-
-
-def get_inner_model_itemid(itemid):
-    return itemid_mapping.get(itemid)
-
-
-def get_original_visitorid(inner_visitorid):
-    return visitorid_labels.get(inner_visitorid)
-
-
-def get_original_itemid(inner_itemid):
-    return itemid_labels.get(inner_itemid)
-
-
-def generate_top5_recommendations():
-    pass
-'''
 
 def user_checker(visitorid):
     return visitorid in all_users
@@ -44,3 +24,4 @@ def get_predictions(visitorid):
     pred_scores = lfm_model.predict(visitorid, item_sparse)
     top_scores = np.argsort(-pred_scores)[:3]
     return all_items[top_scores]
+
